c771fa95d8b1734e4ecef253e904ec16adab52b2
AlopexTMNT <74680084+AlopexTMNT@users.noreply.github.com>
2022-07-08 10:30:14 +0300

    Add files via upload

--- config.h
+++ config.h
@@ -93,10 +93,11 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,	XK_F5,	spawn,	SHCMD("setxkbmap us")	},
 	{ MODKEY,	XK_F6,	spawn,	SHCMD("setxkbmap gr")	},
-	{ MODKEY,	XK_w,	spawn,	SHCMD("librewolf") },
+	{ MODKEY,	XK_w,	spawn,	SHCMD("firefox") },
 	{ 0,		XK_Print,	spawn,	SHCMD("cd Pictures && maim pic-full-$(date '+%y%m%d-%H%M-%S').png") },
 	{ MODKEY,       XK_r,           spawn,  SHCMD("cd Videos/Record && ffmpeg -f x11grab -video_size 1366x768 -framerate 25 -i $DISPLAY -f alsa -i default -c:v libx264 -preset ultrafast -c:a aac rec-$(date '+%y%m%d-%H%M-%S').mp4") },
         { MODKEY|ShiftMask,     XK_r,   spawn,  SHCMD("killall ffmpeg") },
+        { MODKEY|ShiftMask,     XK_s,   spawn,  SHCMD(". ~/.scripts/./poweroptions.sh") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
